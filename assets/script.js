d328892388086021f909056cc39c8a4b5a9c0fdc
Jruuuu <71367882+Jruuuu@users.noreply.github.com>
2020-10-21 00:28:25 -0700

    made changes to navbar and added psuedo code for javascript

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+
+
+//Eventlistner "click, start button
+
+//funcition event(start timer) and question #1 pop ups with four answers. button A, Button B, Button C, and Button D.
+
+// Correct answer moves to the next question
+
+//Wrong answer Deducts time from timer
+
+//When all questions are answered or the time =0 Game is over
+
+//When game is over you can track your score by attaching your intials. 
