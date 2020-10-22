1e8ab75493312929cf496b6ff7f31584d78d1b7c
Jruuuu <71367882+Jruuuu@users.noreply.github.com>
2020-10-21 17:22:26 -0700

    worked on Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-
-
 //Eventlistner "click, start button
 
 //funcition event(start timer) and question #1 pop ups with four answers. button A, Button B, Button C, and Button D.
@@ -11,3 +9,131 @@
 //When all questions are answered or the time =0 Game is over
 
 //When game is over you can track your score by attaching your intials. 
+
+var timer;
+var time = 60;
+var questionIndex = 0;
+var score = 0;
+
+var questions = [
+    {
+        questionText: "What can be in []",
+        choice: ["string", "array", "var", "dog"],
+        answer: "array"
+    },
+    {
+        questionText: "This is q2?",
+        choice: ["cat", "dog", "mouse", "fish"],
+        answer: "dog"
+    },
+    {
+        questionText: "This is q3?",
+        choice: ["cat", "dog", "mouse", "fish"],
+        answer: "mouse"
+    }
+]
+
+// click start quiz then the quiz should start
+function startQuiz() {
+    //hide start button
+    document.querySelector(".start-container").classList.add("hide");
+
+    //timer starts
+    timer = setInterval(function() {
+        time--;
+        document.querySelector(".timer span").textContent = time;
+        //check if time is 0 if so end quiz
+        if (time <= 0) {
+            endQuiz();
+        };
+    },1000);
+
+    //shows question container
+    document.querySelector(".question-container").classList.remove("hide");
+
+    //create first question
+    generateQuestion();
+};
+
+function generateQuestion() {
+    //create question html
+    var questionHTML = `
+    <div id="question" >
+        <div id="questionTitle">${questions[questionIndex].questionText}</div>
+        <div id="answerChocies" class="ansLayout">
+            <button class="btn">${questions[questionIndex].choice[0]}</button>
+            <button class="btn">${questions[questionIndex].choice[1]}</button>
+            <button class="btn">${questions[questionIndex].choice[2]}</button>
+            <button class="btn">${questions[questionIndex].choice[3]}</button>
+        </div>
+    </div>
+    `;
+    //inject the question into quetion container
+    document.querySelector(".question-container").innerHTML = questionHTML;
+};
+
+function endQuiz() {
+    //hide question container
+    document.querySelector(".question-container").classList.add("hide");
+
+    //stop the timer
+    clearInterval(timer);
+
+    //show end html
+    document.querySelector(".end-markup").classList.remove("hide");
+
+    //show final score
+    document.querySelector("#final-score").textContent = score;
+}
+
+function processAnswer(event) {
+    //check if answer is correct
+    if (event.target.textContent === questions[questionIndex].answer) {
+        //increase score
+        score++;
+    } else {
+        time = time - 5
+    }
+    //prepare to show the next question
+    questionIndex++;
+
+    //check to see if we should end quiz
+    if (questionIndex === questions.length) {
+        endQuiz();
+    }
+
+    //show next question
+    generateQuestion();
+}
+
+//add event listener to start btn
+document.querySelector("#start-btn").addEventListener("click", startQuiz);
+
+//add event listener to each answer choice use parent as event delegator
+document.querySelector(".question-container").addEventListener("click", function(event) {
+    //make the function only runs when btn class is clicked
+    if (event.target.className.indexOf("btn") > -1) {
+        processAnswer(event);
+    }
+});
+
+
+//on submit button click
+
+    //pull out data from local storage
+    //parse it
+
+    //save initials, time, score
+    //put it in an object format
+
+    //push it into pulled out data
+
+    //stringify the object
+
+    //store the object in local stoarge
+
+    //change window.location to highscore html page
+
+
+    //other page code 
+        //pull local storage data and render it onto the page
